088b123e4a50c16e24128764329b6872ee4f3525
Thiago Felipe Festa <thiagofesta@gmail.com>
2015-09-12 15:12:16 -0300

    Releasing

--- release/tf-metatags.js
+++ release/tf-metatags.js
@@ -1,6 +1,6 @@
 /**
  * Angular module for providing MetaTags support based on routes.
- * @version v0.3.2-dev-2015-09-12
+ * @version v0.4.0-dev-2015-09-12
  * @link https://github.com/thiagofesta/tf-metatags
  * @author Thiago Festa <thiagofesta@gmail.com> (http://thiagofesta.com)
  * @license MIT License, http://www.opensource.org/licenses/MIT
